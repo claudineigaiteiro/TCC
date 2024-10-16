539c0b37c20f1d6deefcb59a196690c40e32956e
claudinei <claudineigaiteiro@gmail.com>
2024-10-15 21:08:49 -0300

    #CRM - Ajuste no grafico pluviometro.

--- Client/src/View.Pluviometro.dfm
+++ Client/src/View.Pluviometro.dfm
@@ -1,10 +1,40 @@
 inherited Pluviometro: TPluviometro
   Caption = 'Pluviometro'
   TextHeight = 15
+  inherited PnlHeader: TPanel
+    inherited BtnGerarDados: TButton
+      OnClick = BtnGerarDadosClick
+    end
+  end
   inherited PnlBody: TPanel
     ExplicitLeft = 0
     ExplicitTop = 73
     ExplicitWidth = 624
     ExplicitHeight = 311
+    object WebBrowser1: TWebBrowser
+      Left = 1
+      Top = 1
+      Width = 622
+      Height = 309
+      Align = alClient
+      TabOrder = 0
+      ControlData = {
+        4C00000049400000F01F00000000000000000000000000000000000000000000
+        000000004C000000000000000000000001000000E0D057007335CF11AE690800
+        2B2E126208000000000000004C0000000114020000000000C000000000000046
+        8000000000000000000000000000000000000000000000000000000000000000
+        00000000000000000100000000000000000000000000000000000000}
+    end
+  end
+  object mtGrafico: TFDMemTable
+    FetchOptions.AssignedValues = [evMode]
+    FetchOptions.Mode = fmAll
+    ResourceOptions.AssignedValues = [rvSilentMode]
+    ResourceOptions.SilentMode = True
+    UpdateOptions.AssignedValues = [uvCheckRequired, uvAutoCommitUpdates]
+    UpdateOptions.CheckRequired = False
+    UpdateOptions.AutoCommitUpdates = True
+    Left = 544
+    Top = 152
   end
 end
--- Client/src/View.Pluviometro.pas
+++ Client/src/View.Pluviometro.pas
@@ -6,10 +6,17 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, View.FormBaseDemonstracao, Vcl.StdCtrls,
-  Vcl.Mask, Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.ComCtrls, System.Sensors, System.Sensors.Components;
+  Vcl.Mask, Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.ComCtrls, System.Sensors,
+  System.Sensors.Components, Vcl.OleCtrls, SHDocVw, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
+  FireDAC.Phys.Intf, FireDAC.DApt.Intf, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TPluviometro = class(TfrmBaseDesmonstracao)
+    WebBrowser1: TWebBrowser;
+    mtGrafico: TFDMemTable;
+    procedure BtnGerarDadosClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,6 +32,30 @@ implementation
 { TPluviometro }
 
 uses RESTRequest4D, System.JSON;
+
+procedure TPluviometro.BtnGerarDadosClick(Sender: TObject);
+var
+  LJsonStream: TStringStream;
+begin
+  inherited;
+  case RgTipoBusca.ItemIndex of
+    0:
+      begin
+        LJsonStream := TStringStream.Create(getLeituraDiaria('1', tpDataInicio.Date));
+        Try
+          mtGrafico.Close;
+          mtGrafico.LoadFromStream(LJsonStream);
+        Finally
+          FreeAndNil(LJsonStream);
+        End;
+
+      end;
+    1:
+      begin
+
+      end;
+  end;
+end;
 
 class function TPluviometro.getLeituraDiaria(AIdUnidade: String;
   AData: TDate): String;
